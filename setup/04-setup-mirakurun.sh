415bcdf324c5f89de118dc76bf6086b26e41ed55
け <challengerk.8000@gmail.com>
2023-12-20 20:49:37 +0900

    Update 04-setup-mirakurun.sh

--- setup/04-setup-mirakurun.sh
+++ setup/04-setup-mirakurun.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Node.js v18のインストール
-NODE_MAJOR=18
+# Node.jsのインストール準備
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+# Node.js v16のインストール
+NODE_MAJOR=16
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install nodejs -y
+nodejs -v
 
 # PM2のインストール
 sudo npm install pm2 --location=global
@@ -15,6 +18,12 @@ sudo pm2 startup
 # Mirakurunのインストール
 sudo npm install mirakurun@3.8.1 --location=global --production
 sudo mirakurun init
+
+# Node.js v18のインストール（Mirakurun v3.8.1経由）
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 # v3.8.1を経由しないと3.9.xに上げられない
 sudo npm install mirakurun --location=global --production
