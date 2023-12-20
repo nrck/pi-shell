445c0afebda0e0438188dd144a9785c599782025
け <challengerk.8000@gmail.com>
2023-12-20 19:07:35 +0900

    update: Node.js 18をインストールするように変更

--- setup/04-setup-mirakurun.sh
+++ setup/04-setup-mirakurun.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Node.js v16のインストール
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Node.js v18のインストール
+NODE_MAJOR=18
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 # PM2のインストール
 sudo npm install pm2 --location=global
