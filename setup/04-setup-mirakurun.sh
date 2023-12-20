9b3485a40407342467c214ee8a8df7476c0f4dde
け <challengerk.8000@gmail.com>
2023-12-20 21:31:26 +0900

    Update 04-setup-mirakurun.sh

--- setup/04-setup-mirakurun.sh
+++ setup/04-setup-mirakurun.sh
@@ -8,7 +8,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=16
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
-sudo apt-get install nodejs -y
+sudo apt-get install nodejs=16.20.2-1nodesource1 -y
 nodejs -v
 
 # PM2のインストール
@@ -38,9 +38,9 @@ echo -n "*** npm Version: "
 npm -v
 
 # PM2のバージョン確認
-echo "*** pm2 Version: "
-pm2 -v
+echo -n "*** pm2 Version: "
+pm2 -V
 
 # Mirakurunのバージョン確認
-echo "*** Mirakurun Version: "
-sudo mirakurun version
+echo -n "*** Mirakurun Version: "
+sudo mirakurun version | grep mirakurun
