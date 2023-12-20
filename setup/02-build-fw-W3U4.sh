3bf41293787855e5904fe5410e84c3d54790f703
け <challengerk.8000@gmail.com>
2023-12-20 18:56:08 +0900

    update: DKMSを使用したドライバのインストールに変更

--- setup/02-build-fw-W3U4.sh
+++ setup/02-build-fw-W3U4.sh
@@ -12,6 +12,10 @@ make
 
 # PX-W3U4ドライバのダウンロード
 wget http://plex-net.co.jp/download/pxw3u4v1.4.zip -O pxw3u4v1.4.zip
+if [ $? -ne 0 ]; then
+    echo "Error: pxw3u4v1.4.zip download"
+    exit 1
+fi
 
 # PX-W3U4ドライバの解凍
 unzip -oj pxw3u4v1.4.zip pxw3u4v1/x64/PXW3U4.sys
@@ -27,11 +31,23 @@ sudo mv it930x-firmware_pxw3u4.bin /lib/firmware/it930x-firmware.bin
 rm ./pxw3u4v1.4.zip
 rm ./PXW3U4.sys
 
-# ドライバのインストール
-cd ../driver
-make
-sudo make install
+# DKMSを使用してドライバのインストール
+cd ../
+sudo cp -a ./ /usr/src/px4_drv-0.2.1
+sudo dkms add px4_drv/0.2.1
+sudo dkms install px4_drv/0.2.1
+if [ $? -ne 0 ]; then
+  echo "Error: px4_drv install"
+  exit 1
+fi
+
+# カーネルモジュールのロードの確認
 sudo modprobe px4_drv
+lsmod | grep -e ^px4_drv
+if [ $? -ne 0 ]; then
+  echo "Error: modprobe px4_drv"
+  exit 1
+fi
 
 # 環境設定
 cat /boot/cmdline.txt | grep coherent_pool=4M
